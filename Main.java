ea69a30285ac5ec52930ee41dcf596f036d09585
draveau13 <Q0e5pktt;15g5>
2021-05-28 00:43:39 +0200

    2 test espace

--- Main.java
+++ Main.java
@@ -1,13 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("salut");
-    }
-
-    public String fonctionDev() {
-        return "dev 2";
-    }
-
-    public String fonctionTest() {
-        return "     test 2";
-    }
+    public static void main(String[] args) { System.out.println("salut"); }
+    public String fonctionDev() { return "dev 2"; }
+    public String fonctionTest() {  return "     test 2"; }
 }
