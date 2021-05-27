700a71d45eaa7dbe889232f45fbc054ee9b9a4e7
draveau13 <Q0e5pktt;15g5>
2021-05-28 00:44:17 +0200

    2 test espace

--- Main.java
+++ Main.java
@@ -1,13 +1,28 @@
 public class Main {
     public static void main(String[] args) {
+
+
         System.out.println("salut");
+
+
+
     }
 
     public String fonctionDev() {
+
+
         return "dev 2";
+
+
     }
 
     public String fonctionTest() {
+
+
         return "     test 2";
+
+
+
+
     }
 }
